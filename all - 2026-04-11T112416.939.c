3574c84a6c6625fee3073df66e064d5cd8ec7f31
Rudraksh Chikara <rudrakshachikara@gmail.com>
2026-04-11 11:24:45 +0530

    Day 70

---
+++ all - 2026-04-11T112416.939.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+typedef struct {
+    int u, v, w;
+} Edge;
+
+void bellmanFord(int n, int m, Edge edges[], int src) {
+    int dist[n];
+    for (int i = 0; i < n; i++) dist[i] = INT_MAX;
+    dist[src] = 0;
+
+    // Relax edges n-1 times
+    for (int i = 1; i <= n-1; i++) {
+        for (int j = 0; j < m; j++) {
+            int u = edges[j].u;
+            int v = edges[j].v;
+            int w = edges[j].w;
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    // Check for negative weight cycle
+    for (int j = 0; j < m; j++) {
+        int u = edges[j].u;
+        int v = edges[j].v;
+        int w = edges[j].w;
+        if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+            printf("NEGATIVE CYCLE\n");
+            return;
+        }
+    }
+
+    // Print distances
+    printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < n; i++) {
+        if (dist[i] == INT_MAX)
+            printf("%d \t\t INF\n", i);
+        else
+            printf("%d \t\t %d\n", i, dist[i]);
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    Edge edges[m];
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
+    }
+    int src;
+    scanf("%d", &src);
+
+    bellmanFord(n, m, edges, src);
+    return 0;
+}
+
